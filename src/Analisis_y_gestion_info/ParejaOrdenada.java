cd1d10beb1dde037bbe671a402a2d3ea94fecdd8
aalvaroo7 <almarrom@myuax.com>
2024-04-22 16:02:10 +0200

    comit

--- src/Analisis_y_gestion_info/ParejaOrdenada.java
+++ src/Analisis_y_gestion_info/ParejaOrdenada.java
@@ -2,14 +2,14 @@ package Analisis_y_gestion_info;
 
 import Gestio_datos_dinamicos.Pareja;
 
-public class ParejaOrdenada<T extends Comparable<T>, U extends Comparable<U>> extends Pareja<T, U> implements Comparable<ParejaOrdenada<T, U>> {
+public class ParejaOrdenada<T extends Comparable<T>, U extends Comparable<U>> extends Pareja<T, U> {
 
     public ParejaOrdenada(T first, U second) {
         super(first, second);
     }
 
     @Override
-    public int compareTo(ParejaOrdenada<T, U> otraPareja) {
+    public int compareTo(Pareja<T, U> otraPareja) {
         int comparacionPrimerElemento = this.getFirst().compareTo(otraPareja.getFirst());
         if (comparacionPrimerElemento != 0) {
             return comparacionPrimerElemento;
