d4b0ccdccf4e117d71c1a5ef3bcea2445075196b
aalvaroo7 <almarrom@myuax.com>
2024-04-22 16:30:56 +0200

    comit

--- src/Gestio_datos_dinamicos/InterfazUsuario.java
+++ src/Gestio_datos_dinamicos/InterfazUsuario.java
@@ -85,15 +85,4 @@ public class InterfazUsuario extends JFrame {
             areaTexto.append(dato + "\n");
         }
     }
-    // En la clase InterfazUsuario
-botonEliminar.addActionListener(e -> {
-        // Eliminar el último elemento de la lista
-        if (!listaDatos.isEmpty()) {
-            listaDatos.remove(listaDatos.size() - 1);
-
-            // Actualizar el área de texto
-            actualizarAreaTexto();
-        }
-    });
-
 }
